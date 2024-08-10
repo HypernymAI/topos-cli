3a4d77e64723c756478ba3076dd60da4cbb9e713
anchellon <anshulmathew@gmail.com>
2024-08-10 10:41:41 -0700

    added gpt-4o support

--- topos/generations/ollama_chat.py
+++ topos/generations/ollama_chat.py
@@ -1,14 +1,17 @@
+# https://platform.openai.com/docs/libraries/python-library
+# setting the env var in a user data script in aws
 from openai import OpenAI
-
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
+print(os.getenv('OPENAI_API_KEY'))
 client = OpenAI(
-    base_url='http://localhost:11434/v1',
-    api_key='ollama',  # required, but unused
-)
+    api_key=os.getenv('OPENAI_API_KEY')
+    )
 
-def stream_chat(message_history, model="solar", temperature=0):
+def stream_chat(message_history, model="gpt-4o", temperature=0):
     response = client.chat.completions.create(
-        model=model,
+        model="gpt-4o",
         messages=message_history,
         temperature=temperature,
         stream=True
@@ -17,9 +20,9 @@ def stream_chat(message_history, model="solar", temperature=0):
         if chunk.choices[0].delta.content is not None:
             yield chunk.choices[0].delta.content
 
-def generate_response(context, prompt, model="solar", temperature=0):
+def generate_response(context, prompt, model="gpt-4o", temperature=0):
     response = client.chat.completions.create(
-        model=model,
+        model="gpt-4o",
         messages=[
             {"role": "system", "content": context},
             {"role": "user", "content": prompt}
@@ -28,9 +31,9 @@ def generate_response(context, prompt, model="solar", temperature=0):
     )
     return response.choices[0].message.content
 
-def generate_response_messages(message_history, model="solar", temperature=0):
+def generate_response_messages(message_history, model="gpt-4o", temperature=0):
     response = client.chat.completions.create(
-        model=model,
+        model="gpt-4o",
         messages=message_history,
         stream=False
     )
