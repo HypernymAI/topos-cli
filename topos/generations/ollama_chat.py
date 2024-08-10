b2c004ec7b5ce00958909f22da3b79670a6b2310
anchellon <anshulmathew@gmail.com>
2024-08-10 12:06:28 -0700

    set the env var in aws user data script

--- topos/generations/ollama_chat.py
+++ topos/generations/ollama_chat.py
@@ -1,13 +1,7 @@
 # https://platform.openai.com/docs/libraries/python-library
 # setting the env var in a user data script in aws
 from openai import OpenAI
-import os
-from dotenv import load_dotenv
-load_dotenv()
-print(os.getenv('OPENAI_API_KEY'))
-client = OpenAI(
-    api_key=os.getenv('OPENAI_API_KEY')
-    )
+client = OpenAI()
 
 def stream_chat(message_history, model="gpt-4o", temperature=0):
     response = client.chat.completions.create(
